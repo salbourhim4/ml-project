b093ec2e20d33c06ba84e96da8b9ee1d4ce47133
Salaheddine <salbourhim4@gmail.com>
2026-05-10 20:30:33 -0400

    Add matrix operations test cases

--- src/main.c
+++ src/main.c
@@ -1,28 +1,75 @@
 #include <stdio.h>
 #include "matrix.h"
 
-int main(void) {
-    Arena a = arena_create(1024);
-
-    Matrix a_mat = matrix_create(&a, 2, 3);
-    matrix_set(&a_mat, 0, 0, 1); matrix_set(&a_mat, 0, 1, 2); matrix_set(&a_mat, 0, 2, 3);
-    matrix_set(&a_mat, 1, 0, 4); matrix_set(&a_mat, 1, 1, 5); matrix_set(&a_mat, 1, 2, 6);
-
-    Matrix b_mat = matrix_create(&a, 3, 2);
-    matrix_set(&b_mat, 0, 0, 7); matrix_set(&b_mat, 0, 1, 8);
-    matrix_set(&b_mat, 1, 0, 9); matrix_set(&b_mat, 1, 1, 10);
-    matrix_set(&b_mat, 2, 0, 11); matrix_set(&b_mat, 2, 1, 12);
-
-    Matrix result = matrix_create(&a, 2, 2);
-    matrix_mul(&a_mat, &b_mat, &result);
-
-    printf("Result:\n");
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            printf("%.1f ", matrix_get(&result, i, j));
+void print_matrix(Matrix *m) {
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            printf("%.2f ", matrix_get(m, i, j));
         }
         printf("\n");
     }
+    printf("\n");
+}
+
+int main(void) {
+    Arena a = arena_create(1024 * 1024);
+
+    // test matrices
+    Matrix m1 = matrix_create(&a, 2, 2);
+    matrix_set(&m1, 0, 0, 1); matrix_set(&m1, 0, 1, 2);
+    matrix_set(&m1, 1, 0, 3); matrix_set(&m1, 1, 1, 4);
+
+    Matrix m2 = matrix_create(&a, 2, 2);
+    matrix_set(&m2, 0, 0, 5); matrix_set(&m2, 0, 1, 6);
+    matrix_set(&m2, 1, 0, 7); matrix_set(&m2, 1, 1, 8);
+
+    Matrix result = matrix_create(&a, 2, 2);
+
+    // test add
+    matrix_add(&m1, &m2, &result);
+    printf("Add (expect 6 8 / 10 12):\n");
+    print_matrix(&result);
+
+    // test sub
+    matrix_sub(&m2, &m1, &result);
+    printf("Sub (expect 4 4 / 4 4):\n");
+    print_matrix(&result);
+
+    // test mul
+    matrix_mul(&m1, &m2, &result);
+    printf("Mul (expect 19 22 / 43 50):\n");
+    print_matrix(&result);
+
+    // test scale
+    matrix_scale(&m1, 2.0, &result);
+    printf("Scale (expect 2 4 / 6 8):\n");
+    print_matrix(&result);
+
+    // test transpose
+    Matrix t = matrix_create(&a, 2, 2);
+    matrix_transpose(&m1, &t);
+    printf("Transpose (expect 1 3 / 2 4):\n");
+    print_matrix(&t);
+
+    // test elementwise mul
+    matrix_elementwise_mul(&m1, &m2, &result);
+    printf("Elementwise (expect 5 12 / 21 32):\n");
+    print_matrix(&result);
+
+    // test zero
+    matrix_zero(&result);
+    printf("Zero (expect 0 0 / 0 0):\n");
+    print_matrix(&result);
+
+    // test copy
+    matrix_copy(&m1, &result);
+    printf("Copy (expect 1 2 / 3 4):\n");
+    print_matrix(&result);
+
+    // test randomize
+    matrix_randomize(&result, -1.0, 1.0);
+    printf("Randomize (expect values between -1 and 1):\n");
+    print_matrix(&result);
 
     arena_destroy(&a);
     return 0;
