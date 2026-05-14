e9bfe5f5e2261b6848d5f578c197b4a5d2562bd0
Salaheddine <salbourhim4@gmail.com>
2026-05-14 17:33:47 -0400

    Refactor backprop into helper functions for dE, dZ, dW, dA

--- src/nn.h
+++ src/nn.h
@@ -17,8 +17,15 @@ float relu_derivative(float x);
 float sigmoid_derivative(float x);
 
 Network network_create(Arena *a, int *layer_sizes, int num_layers);
-void network_forward(Network *net, Matrix *input, Matrix *zs, Arena *scratch, float (*activation)(float));
-void network_backward(Network *net, Matrix *output, Matrix *labels, Matrix *zs, Arena *scratch, float (*derivative)(float));//Labels from mnist
+void network_forward(Network *net, Matrix *input, Matrix *zs, Matrix *as, Arena *scratch, float (*activation)(float));
+
+// let each aspect of backprop be its own function instead of writing all of them in network_backward
+Matrix compute_dE(Matrix *output, Matrix *labels, Arena *scratch);
+Matrix compute_dZ(Matrix *error, Matrix *z, Arena *scratch, float (*derivative)(float));
+Matrix compute_dW(Matrix *dZ, Matrix *input, Arena *scratch);
+Matrix compute_dA(Matrix *weights, Matrix *dZ, Arena *scratch);
+
+void network_backward(Network *net, Matrix *input, Matrix *output, Matrix *labels, Matrix *zs, Matrix *as, Arena *scratch, float (*derivative)(float));//Labels from mnist
 void network_update_weights(Network *net, float learning_rate);
 void network_free(Network *n);
 float cross_entropy(Matrix *output, Matrix *labels);
